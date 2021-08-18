459b2fa1c064b9e392f75eac8320397ad0eb8ef7
Dasha Kurokhtina <74241498+yourtastylemon@users.noreply.github.com>
2021-08-18 22:29:06 +0300

    Add files via upload

---
+++ trafficLight/trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by Дарья Курохтина on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
